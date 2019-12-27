6ae2a5a0e384fa221e540d63c50da20a57144cd0
DESKTOP-8MGJ15I\Faraday <aron-freyer@gmx.de>
2019-12-28 00:59:26 +0100

    fixed 09_02

---
+++ 09_cpp/09_02/09_02_main.cpp
@@ -0,0 +1,10 @@
+#include "intcode_computer.h"
+
+int main()
+{
+    uint8_t input = 2;
+    CIntcode_computer intcoder;
+    intcoder.run_programm(input);
+    cout << "------------------\n";
+    cout << "program ends";
+}
